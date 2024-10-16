bb6419a43decb0793d541e6d749c16ebac7055d3
ananddecafit <anand.decafit@gmail.com>
2024-10-16 10:07:42 +0530

    db port check

--- be/routes/index.js
+++ be/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Management app API' });
+  res.render('index', { title: `Management app API, db on ${process.env.DB_PORT}` });
 });
 
 module.exports = router;
--- fe/src/app/home/page.tsx
+++ fe/src/app/home/page.tsx
@@ -2,5 +2,5 @@ import { unstable_noStore as noStore } from 'next/cache'
 
 export default function Home() {
     noStore()
-    return <main>PORT={process.env.NEXT_PUBLIC_PORT} API_PORT={process.env.NEXT_PUBLIC_API_PORT}</main>;
+    return <main>PORT={process.env.PORT} API_PORT={process.env.API_PORT}</main>;
 }

e3602e2e5900d53964194959d3783eb569423a61
ananddecafit <anand.decafit@gmail.com>
2024-10-16 12:32:37 +0530

    basic db flow tested

--- be/README.md
+++ be/README.md
@@ -1,7 +1,10 @@
-npm start
+npm install -g knex
 
+npm start
 
 # docker builds
 docker build -t asanand1996/be . 
 
 docker run --name be -d -p 8000:8000 asanand1996/be
+
+knex --connection "postgresql://user:postgres@localhost:5432/mgmt-sys" migrate:latest
---
+++ be/knexfile.js
@@ -0,0 +1,51 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOSTNAME,
+      port: process.env.DB_PORT,
+      user:     process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'mgmt-sys'
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};

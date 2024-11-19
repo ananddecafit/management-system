c4ec5644c14b1105643ff8effa987aab74ae7580
ananddecafit <anand.decafit@gmail.com>
2024-11-19 18:25:03 +0530

    docker flow tested

--- be/src/routes/index.ts
+++ be/src/routes/index.ts
@@ -3,7 +3,8 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: `Management app API, db on ${process.env.DB_PORT}` });
+    // res.render('index', { title: `Management app API, db on ${process.env.DB_PORT}` });
+    res.status(200).send({ title: `Management app API, db on ${process.env.DB_PORT}, name ${process.env.DB_NAME}` });
   });
 
 export default router;

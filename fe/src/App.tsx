4eab26a7655d13da345a55df399ce4582834d31d
ananddecafit <anand.decafit@gmail.com>
2025-01-08 19:28:19 +0530

    basic google login

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,33 +2,45 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getTestData } from './api/test';
+import { CredentialResponse, GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
 
-  const [data, setData] = useState<{title: string}[]>();
+  const [, setData] = useState<{title: string}[]>();
+  const [session, setSession] = useState<CredentialResponse>();
+
+  const errorMessage = () => {
+      console.log("error");
+  };
 
   useEffect(() => {
     getTestData().then((res) => setData(res.data)).catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React PORT= {process.env.REACT_APP_PORT}, API_PATH={process.env.REACT_APP_API_PATH}
-        </a>
-        Data = {data && data[0] && data[0].title}
-      </header>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React PORT= {process.env.REACT_APP_PORT}, Google id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+          </a>
+          {session ? (
+            <div>Login success</div>
+            ) : (
+            <GoogleLogin onSuccess={setSession} onError={errorMessage} useOneTap />
+            )}
+        </header>
     </div>
+    </GoogleOAuthProvider>
   );
 }
 

3db611835fb01ae707ea0c5a67a67ba7d180581c
ananddecafit <anand.decafit@gmail.com>
2025-01-10 10:22:51 +0530

    google sheets apijs code

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,32 +1,124 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { getTestData } from './api/test';
-import { CredentialResponse, GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
-import { AuthContext } from './contexts/AuthContext';
-import Test from './components/Test';
 
 function App() {
+  let gapi: any;
+  /**
+       * Callback after api.js is loaded.
+       */
+  function onGapiLoaded() {
+    console.log(gapi);
+    // gapi.load('client', initializeGapiClient);
+    gapi.load('client', () => {
+      console.log(gapi.client);
+    });
+  }
 
-  const [user, setUser] = useState<any>();
-  const [credentialResponse, setCredentialResponse] = useState<CredentialResponse | undefined>();
+  /**
+   * Callback after the API client is loaded. Loads the
+   * discovery doc to initialize the API.
+   */
+  // async function initializeGapiClient() {
+  //   await gapi.client.init({
+  //     apiKey: API_KEY,
+  //     discoveryDocs: [DISCOVERY_DOC],
+  //   });
+  //   gapiInited = true;
+  //   maybeEnableButtons();
+  // }
 
-  useEffect(() => {
-    if (!user && credentialResponse?.credential) {
-      setUser(jwtDecode(credentialResponse.credential));
-      console.log(user);
-    }
-  }, [credentialResponse, user]);
+  // /**
+  //  * Callback after Google Identity Services are loaded.
+  //  */
+  // function gisLoaded() {
+  //   tokenClient = google.accounts.oauth2.initTokenClient({
+  //     client_id: CLIENT_ID,
+  //     scope: SCOPES,
+  //     callback: '', // defined later
+  //   });
+  //   gisInited = true;
+  //   maybeEnableButtons();
+  // }
 
-  const errorMessage = () => {
-      console.log("error");
-  };
+  // /**
+  //  * Enables user interaction after all libraries are loaded.
+  //  */
+  // function maybeEnableButtons() {
+  //   if (gapiInited && gisInited) {
+  //     document.getElementById('authorize_button').style.visibility = 'visible';
+  //   }
+  // }
+
+  // /**
+  //  *  Sign in the user upon button click.
+  //  */
+  // function handleAuthClick() {
+  //   tokenClient.callback = async (resp) => {
+  //     if (resp.error !== undefined) {
+  //       throw (resp);
+  //     }
+  //     document.getElementById('signout_button').style.visibility = 'visible';
+  //     document.getElementById('authorize_button').innerText = 'Refresh';
+  //     await listMajors();
+  //   };
+
+  //   if (gapi.client.getToken() === null) {
+  //     // Prompt the user to select a Google Account and ask for consent to share their data
+  //     // when establishing a new session.
+  //     tokenClient.requestAccessToken({prompt: 'consent'});
+  //   } else {
+  //     // Skip display of account chooser and consent dialog for an existing session.
+  //     tokenClient.requestAccessToken({prompt: ''});
+  //   }
+  // }
+
+  // /**
+  //  *  Sign out the user upon button click.
+  //  */
+  // function handleSignoutClick() {
+  //   const token = gapi.client.getToken();
+  //   if (token !== null) {
+  //     google.accounts.oauth2.revoke(token.access_token);
+  //     gapi.client.setToken('');
+  //     document.getElementById('content').innerText = '';
+  //     document.getElementById('authorize_button').innerText = 'Authorize';
+  //     document.getElementById('signout_button').style.visibility = 'hidden';
+  //   }
+  // }
+
+  // /**
+  //  * Print the names and majors of students in a sample spreadsheet:
+  //  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+  //  */
+  // async function listMajors() {
+  //   let response;
+  //   try {
+  //     // Fetch first 10 files
+  //     response = await gapi.client.sheets.spreadsheets.values.get({
+  //       spreadsheetId: '15DS3prlfbOpvgGXAKMJq-bZjyxBbqF1y7qrki6mVQB4',
+  //       range: 'USERS!A:B',
+  //     });
+  //   } catch (err) {
+  //     document.getElementById('content').innerText = err.message;
+  //     return;
+  //   }
+  //   const range = response.result;
+  //   if (!range || !range.values || range.values.length == 0) {
+  //     document.getElementById('content').innerText = 'No values found.';
+  //     return;
+  //   }
+  //   // Flatten to string to display
+  //   const output = range.values.reduce(
+  //       (str, row) => `${str}${row[0]}, ${row[1]}\n`,
+  //       '');
+  //   document.getElementById('content').innerText = output;
+  // }
 
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID!}>
-      <AuthContext.Provider value={{ credentialResponse, user }}>
       <div className="App">
+        <script type="text/javascript" src="https://apis.google.com/js/api.js" onLoad={onGapiLoaded}></script>
+        {/* <script async defer src="https://accounts.google.com/gsi/client" onLoad={onGsiLoaded}></script> */}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -40,15 +132,8 @@ function App() {
           >
             Learn React PORT= {process.env.REACT_APP_PORT}, Google id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
           </a>
-          {credentialResponse ? (
-            <Test></Test>
-            ) : (
-            <GoogleLogin onSuccess={setCredentialResponse} onError={errorMessage} useOneTap />
-            )}
         </header>
     </div>
-    </AuthContext.Provider>
-    </GoogleOAuthProvider>
   );
 }
 
